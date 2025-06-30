5d5ce6fefaed30569886d219cca2c5103529ff7a
Abdus Salam Ameen <abdussalamblue7@gmail.com>
2025-06-30 21:27:25 +0500

    Revert "Tweak GUI"
    This reverts commit 4eaa37265c7fab00be5efdce015e6f55cc49de99.
    
    Revert Shadows

--- launcher.py
+++ launcher.py
@@ -50,8 +50,7 @@ class MainWindow(QWidget):
                 letter-spacing: 1px;
             }
             QPushButton:hover {
-                background: rgba(200,200,200,0.8);
-                color = black;
+                background: rgba(0,150,0,0.8);
             }
         """)
         self.launch_btn.clicked.connect(self.launch_simulation)
